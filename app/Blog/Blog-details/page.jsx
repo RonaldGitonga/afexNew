584bed358fc74ae96647bc67e25669360b277048
RonaldGitonga <rgatuiku@gmail.com>
2024-01-03 15:32:42 +0300

    modified:   app/Blog/Blog-details/page.jsx

--- app/Blog/Blog-details/page.jsx
+++ app/Blog/Blog-details/page.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 import Header from '../../components/headerone/Main'
 import Bredcom from '../../components/Bredcom/Main'
 import First from '../../components/menblogdeatils/First'
-import LandingLayout from '../../../LandingLayout'
+
 
 function page() {
   //GET BLOG BY ID
   return (
-    <LandingLayout>
+ 
+    <>
         <Header/>
-        <Bredcom title="Home" subtitle="Blog Details"/>
+        // <Bredcom title="Home" subtitle="Blog Details"/>
         <First/>
-    </LandingLayout>
+        </>
   )
 }
 
