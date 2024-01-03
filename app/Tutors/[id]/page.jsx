085689d4c1e04ba05b35ce5ed6edff109d22831d
RonaldGitonga <rgatuiku@gmail.com>
2024-01-03 15:48:18 +0300

    modified:   app/Tutors/[id]/page.jsx

--- app/Tutors/[id]/page.jsx
+++ app/Tutors/[id]/page.jsx
@@ -1,9 +1,9 @@
 'use client'
 import React from 'react'
-import Header from '../components/headerone/Main'
-import Bredcom from '../components/Bredcom/Main'
-import First from '../components/menteacherdetails/First'
-import LandingLayout from '../../../LandingLayout'
+import Header from '../../components/headerone/Main'
+import Bredcom from '../../components/Bredcom/Main'
+import First from '../../components/menteacherdetails/First'
+import LandingLayout from '../../LandingLayout'
 
 function page() {
   //GET TUTOR BY ID
